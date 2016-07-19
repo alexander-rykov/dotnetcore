2bbb1a94c2539dffd9c5810e9471bebccdd5a09d
Aliaksandr Rykau <aliaksandr_rykau@epam.com>
2016-07-19 17:32:18 +0300

    Add ReadWrite project.

---
+++ Day5_Primitives/ReadWrite/MyList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ReadWrite
+{
+    // TODO: Use ReaderWriterLockSlim to protect this class.
+    public class MyList
+    {
+        private List<int> _list = new List<int>()
+        {
+            1,
+            2,
+            3,
+            4
+        };
+
+        private ReaderWriterLockSlim _readerWriterLock = new ReaderWriterLockSlim();
+
+        public void Add(int value)
+        {
+            Console.WriteLine("Write");
+            _list.Add(value);
+        }
+
+        public void Remove()
+        {
+            if (_list.Count > 0)
+            {
+                Console.WriteLine("Write");
+                _list.RemoveAt(0);
+            }
+        }
+
+        public int Get()
+        {
+            int value = 0;
+            if (_list.Count > 0)
+            {
+                Console.WriteLine("Read");
+                value = _list[0];
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Day5_Primitives/ReadWrite/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ReadWrite
+{
+    class Program
+    {
+        // TODO: replace Object type with appropriate type for slim version of manual reset event.
+        private static IList<Thread> CreateWorkers(Object mres, Action action, int threadsNum, int cycles)
+        {
+            var threads = new Thread[threadsNum];
+
+            for (int i = 0; i < threadsNum; i++)
+            {
+                Action d = () =>
+                {
+                    // TODO: Wait for signal.
+
+                    for (int j = 0; j < cycles; j++)
+                    {
+                        action();
+                    }
+                };
+
+                Thread thread = null; // TODO: Create a new thread that will run the delegate above here.
+
+                threads[i] = thread;
+            }
+
+            return threads;
+        }
+
+        static void Main(string[] args)
+        {
+            var list = new MyList();
+
+            // TODO: Replace Object type with slim version of manual reset event here.
+            Object mres = null;
+
+            var threads = new List<Thread>();
+
+            threads.AddRange(CreateWorkers(mres, () => { list.Add(1); }, 10, 100));
+            threads.AddRange(CreateWorkers(mres, () => { list.Get(); }, 10, 100));
+            threads.AddRange(CreateWorkers(mres, () => { list.Remove(); }, 10, 100));
+
+            foreach (var thread in threads)
+            {
+                // TODO: Start all threads.
+            }
+
+            Console.WriteLine("Press any key to run unblock working threads.");
+            Console.ReadKey();
+
+            // NOTE: When an user presses the key all waiting worker threads should begin their work.
+            // TODO: Send a signal to all worker threads that they can run.
+
+            foreach (var thread in threads)
+            {
+                // TODO: Wait for all working threads
+            }
+
+            Console.WriteLine("Press any key.");
+            Console.ReadKey();
+        }
+    }
+}
