2f36cb9c3d9f4038b8d5e387775400b69d992718
Dmitry Truhanovich <Dmitry_Truhanovich@epam.com>
2017-03-02 08:57:30 +0300

    add tests

---
+++ Day6_Expression/ExpressionTestUnitTest/ExpressionTestUnitTest/ChangeExpressionTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq.Expressions;
+using System.Linq;
+
+namespace Sample02
+{
+    [TestClass]
+    public class ChangeExpressionTest
+    {
+        public class AddToIncrementTransform : ExpressionVisitor
+        {
+            protected override Expression VisitBinary(BinaryExpression node)
+            {
+                if (node.NodeType == ExpressionType.Add)
+                {
+                    ParameterExpression param = null;
+                    ConstantExpression constant = null;
+                    if (node.Left.NodeType == ExpressionType.Parameter)
+                    {
+                        param = (ParameterExpression)node.Left;
+                    }
+                    else if (node.Left.NodeType == ExpressionType.Constant)
+                    {
+                        constant = (ConstantExpression)node.Left;
+                    }
+
+                    if (param != null && constant != null && constant.Type == typeof(int) && (int)constant.Value == 1)
+                    {
+                        return Expression.Increment(param);
+                    }
+                }
+
+                return base.VisitBinary(node);
+            }
+        }
+
+        [TestMethod]
+        public void AddToIncrementTransformTest()
+        {
+            Expression<Func<int, int>> source_exp = (a) => a + (a + 1) * (a + 5) * (a + 1);
+            var result_exp = (new AddToIncrementTransform().VisitAndConvert(source_exp, ""));
+
+            Console.WriteLine(source_exp + " " + source_exp.Compile().Invoke(3));
+            Console.WriteLine(result_exp + " " + result_exp.Compile().Invoke(3));
+        }
+
+
+        public class StringFormatTransform : ExpressionVisitor
+        {
+            protected override Expression VisitMethodCall(MethodCallExpression node)
+            {
+                if (node.Method.DeclaringType == typeof(string) &&
+                    node.Method.Name == "Format" &&
+                    node.Arguments.All(a =>
+                            a.NodeType == ExpressionType.Constant ||
+                            a.NodeType == ExpressionType.Convert &&
+                                ((UnaryExpression)a).Operand.NodeType == ExpressionType.Constant
+                    ))
+                    return Expression.Constant(Expression.Lambda<Func<string>>(node).Compile().Invoke());
+
+                return base.VisitMethodCall(node);
+            }
+        }
+
+
+        [TestMethod]
+        public void StringFormatTransformTest()
+        {
+            Expression<Func<string>> source_exp = () => "3" + "5" + String.Format("String {0} : {1}", 1, "s1");
+            var result_exp = (new StringFormatTransform().VisitAndConvert(source_exp, ""));
+
+            Console.WriteLine(source_exp + " | " + source_exp.Compile().Invoke());
+            Console.WriteLine(result_exp + " | " + result_exp.Compile().Invoke());
+        }
+    }
+}
---
+++ Day6_Expression/UnitTestProjectForConvert/UnitTestProjectForConvert/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using System.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel;
+
+namespace UnitTestProjectForConvert
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EncodeName()
+        {
+            //used XmlConvert for get name 
+            var nameForConvert = "Test\\Name\" for:convert";
+
+            var expected = "Test_x005C_Name_x0022__x0020_for:convert";
+
+            var actual = string.Empty;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DecodeName()
+        {
+            //used XmlConvert for get name 
+            var nameForConvert = "Test_x005C_Name_x0022__x0020_for:convert";
+
+            var expected = "Test\\Name\" for:convert";
+
+            var actual = string.Empty;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsConvertFrom()
+        {
+            //check can convert i to t.
+            TypeConverter t = new TypeConverter();
+            var i = int.MaxValue;
+
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void ConvertTo()
+        {
+            //convert TestClassA to string
+            TypeConverter t = new TypeConverter();
+        }
+
+        [TestMethod]
+        public void ConvertToInt()
+        {
+            //use Convert
+            var startValue = "123";
+            var expected = 123;
+            var actual = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanConvertToInt()
+        {
+            //use Convert in type int
+            var startValue = "123";
+            var expected = 123;
+            var actual = 0;
+            var result = false;
+
+
+            Assert.AreEqual(expected, actual);
+            Assert.IsTrue(result);
+        }
+    }
+}
