6beeb02ae3d05f1b598cf9e852dd03a88bd74029
Aliaksandr Rykau <aliaksandr_rykau@epam.com>
2017-03-16 20:38:39 +0300

    Add GcGenerations.

---
+++ Day3/GcGenerations/First.cs
@@ -0,0 +1,18 @@
+﻿namespace GcGenerations
+{
+    public class First
+    {
+    }
+
+    public class Second
+    {
+        public First First { get; set; }
+    }
+
+    public class Third
+    {
+        public First First { get; set; }
+
+        public Second Second { get; set; }
+    }
+}
---
+++ Day3/GcGenerations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GcGenerations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = new First();
+
+            Console.WriteLine("First is in {0} gen.", GC.GetGeneration(first));
+            Console.WriteLine("Press any key to start GC.");
+            Console.ReadKey();
+            GC.Collect();
+
+            var second = new Second
+            {
+                First = first
+            };
+
+            Console.WriteLine();
+            Console.WriteLine("First is in {0} gen.", GC.GetGeneration(first));
+            Console.WriteLine("Second is in {0} gen.", GC.GetGeneration(second));
+            Console.WriteLine("Press any key to start GC.");
+            Console.ReadKey();
+
+            GC.Collect();
+
+
+            var third = new Third
+            {
+                First = first,
+                Second = second
+            };
+
+            Console.WriteLine();
+            Console.WriteLine("First is in {0} gen.", GC.GetGeneration(first));
+            Console.WriteLine("Second is in {0} gen.", GC.GetGeneration(second));
+
+            var t = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Third is in {0} gen.", GC.GetGeneration(third));
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            });
+
+            t.Wait();
+        }
+    }
+}
